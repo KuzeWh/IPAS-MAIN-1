9e778b47493d95d178e7c61e5127e28d9125baa4
Darie` <prayogadariusw@gmail.com>
2024-11-18 12:26:21 +0700

    Add files via upload

---
+++ Project-PJBL-smt-1-main/index.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Beranda SMPN 2 Malang</title>
+  <!-- CSS Bootstrap -->
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css" rel="stylesheet">
+  <!-- Font -->
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600&display=swap" rel="stylesheet">
+  <!-- Ikon Bootstrap -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.3/font/bootstrap-icons.min.css">
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<?php
+$page = isset($_GET['page'])?$_GET['page'] : 'home';
+?>
+
+<!-- Navbar -->
+<nav class="navbar navbar-expand-lg bg-light sticky-top">
+  <div class="container">
+    <!-- Logo -->
+    <a class="navbar-brand" href="berandasmp.html">
+      <img src="../project kelas 1/Beranda/Image.png" alt="Logo">
+    </a>
+    <!-- hamburger -->
+    <button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <!-- Menu Navbar -->
+        <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+            <li class="nav-item"><a class="nav-link <?= ($page == 'beranda') ? 'active' : '' ?>" href="index.php?page=beranda">Beranda</a></li>
+            <li class="nav-item"><a class="nav-link <?= ($page == 'profil') ? 'active' : '' ?>" href="index.php?page=profil">Profil</a></li>
+            <li class="nav-item"><a class="nav-link <?= ($page == 'prestasi') ? 'active' : '' ?>" href="index.php?page=prestasi">Prestasi</a></li>
+            <li class="nav-item"><a class="nav-link <?= ($page == 'kesiswaan') ? 'active' : '' ?>" href="index.php?page=kesiswaan">Kesiswaan</a></li>
+            <li class="nav-item"><a class="nav-link <?= ($page == 'galeri') ? 'active' : '' ?>" href="index.php?page=galeri">Galeri</a></li>
+            <li class="nav-item"><a class="nav-link <?= ($page == 'sarpras') ? 'active' : '' ?>" href="index.php?page=sarpras">Sarpras</a></li>
+            <li class="nav-item"><a class="nav-link <?= ($page == 'berita') ? 'active' : '' ?>" href="index.php?page=berita">Berita</a></li>
+            <li class="nav-item"><a class="nav-link <?= ($page == 'kontak') ? 'active' : '' ?>" href="index.php?page=kontak">Kontak</a></li>
+            <li class="nav-item ms-3">
+            <a class="nav-link p-0" href="#">
+                <div class="search-icon">
+                    <i class="bi bi-search"></i>
+                </div>
+            </a>
+            </li>
+        </ul>
+        </div>
+  </div>
+</nav>
+
+    <?php
+    switch ($page) {
+        case 'beranda':
+            include 'pages/beranda.php';
+            break;
+        case 'profil':
+            include 'pages/profil.php';
+            break;
+        case 'prestasi':
+            include 'pages/prestasi.php';
+            break;
+        case 'kesiswaan':
+            include 'pages/kesiswaan.php';
+            break;
+        case 'galeri':
+            include 'pages/galeri.php';
+            break;
+        case 'sarpras':
+            include 'pages/sarpras.php';
+            break;
+        case 'berita':
+            include 'pages/berita.php';
+            break;
+        case 'guru':
+            include 'pages/guru.php';
+            break;
+        case 'ekstra':
+            include 'pages/ekstra.php';
+            break;
+        case 'kontak':
+            include 'pages/kontak.php';
+            break;
+        default:
+            echo"<p>Halaman tidak ditemukan</p>";
+            break;
+    }
+    ?>
+
+  <!--footer-->
+  <footer class="footer">
+    <div class="container">
+        <div class="row text-center text-md-start">
+            <div class="col-md-3 mb-3 header-foot">
+                <a href="index.php?page=beranda"><img src="image.png"></a><br><br>
+                <p class="vismi">Unggul Dalam Imtaq dan Iptek, Berkarakter, Serta Peduli dan Berbudaya Lingkungan</p>
+            </div>
+            <div class="col-md-3 mb-3 header-foot">
+                <h5>Alamat</h5><br><br>
+                <p>Jl. Prof. Moch Yamin No. 60, Sukoharjo, Kec. Klojen,</p>
+                <p> Kota Malang, Jawa Timur</p>
+                <p>65118</p>
+            </div>
+            <div class="col-md-3 mb-3 header-foot">
+                <h5>Kontak</h5><br><br>
+                <p>(0341) 325508</p>
+                <p>smpn2mlg.medsos@gmail.com</p>
+            </div>
+            <div class="col-md-3 mb-3 header-foot">
+                <h5>Kantor</h5><br><br>
+                <p class="dino">Senin - Jumat</p>
+                <p class="dino">07.00 - 16.00</p>
+            </div>
+        </div>
+        <div class="row bottom-row">
+            <div class="col-md-6 d-flex justify-content-start">
+                <div class="social-icons">
+                  <a href="https://www.facebook.com/osis.satwimaba.1"><i class="bi bi-facebook"></i></a>
+                  <a href="https://www.youtube.com/channel/UCwuOyXafaSoxICVCQKu1BQg"><i class="bi bi-youtube"></i></a>
+                  <a href="https://www.instagram.com/smpnegeri2mlg/?igshid=13omrlhk8pdjp"><i class="bi bi-instagram"></i></a>
+                  <a href="https://x.com/smpn2mlg"><i class="bi bi-twitter"></i></a>
+                </div>
+            </div>
+            <div class="col-md-6 d-flex justify-content-end">
+                <p class="copyright">&copy; 2024 All Rights Reserved</p>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<!-- Bootstrap Icons -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.8.1/font/bootstrap-icons.min.css">  
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
---
+++ Project-PJBL-smt-1-main/pages/berita.php
@@ -0,0 +1,109 @@
+<style>
+    .berita-title {
+        font-size: 35px;
+        color: #343a40;
+        margin-bottom: 20px;
+        text-align: center;
+        margin-top: 5%;
+        font-family: 'montserrat', sans-serif;
+        font-weight: bold;
+    }
+    .garis-bawah{
+        margin: 0 auto;
+        width: 30%;
+        border: none;
+        height: 2px;
+        background-color: #E04E4E;
+        margin-bottom: 90px;
+    }
+    .text-berita1 {
+        background-color: #E04E4E;
+        border-radius: 20px;
+        margin-left: 4%;
+    }
+    .text-berita1 p{
+        font-family:'montserrat', sans-serif;
+        color: white;
+        font-size: 20px;
+        position: relative;
+        top: 50px;
+        padding-left: 20px;
+        padding-right: 20px;
+    }
+    .text-berita2 {
+        background-color: #E04E4E;
+        border-radius: 20px;
+        margin-right: 4%;
+    }
+    .text-berita2 p{
+        font-family:'montserrat', sans-serif;
+        color: white;
+        font-size: 20px;
+        position: relative;
+        top: 50px;
+        padding-left: 20px;
+        padding-right: 20px;
+    }
+    .image-container img{
+        width: 100%;
+    }
+
+</style>
+<body>
+  
+ <div class="hero-image-container">
+    <img src="../../project kelas 1/berita/Hero (3).png" class="img-fluid w-100" alt="Hero Image">
+</div>
+
+<!--Berita title-->
+<h1 class="berita-title">
+   Berita
+</h1>
+<hr class="garis-bawah">
+
+<!--isi berita-->
+<div class="container berita">
+    <div class="row justify-content-center image-container">
+        <div class="col-md-5 mb-5">
+            <img src="../../project kelas 1/berita/Card (3).png">
+        </div>
+        <div class="col-md-5 mb-5 text-berita1">
+            <p style="padding-top: 30px;"><strong>SMPN 2 Malang</strong>  kelas IX melakukan Kemah blok selama 2 hari 1 malam tapel 2023-2024 sebagai syarat nilai pramuka kelas IX</p>
+        </div>
+        <div class="col-md-5 mb-5 text-berita2">
+           <p><strong>SMPN 2 Malang</strong> melakukan penandatangan <strong>deklarasi anti kekerasan</strong><br>
+            yang dilakukan oleh kepala sekolah,  guru, walikota, dan perwakilan siswa pada tanggal 12 Juni 2024 </p>
+        </div>
+        <div class="col-md-5 mb-5 ">
+           <img src="../../project kelas 1/berita/image (4).png">
+        </div>
+        <div class="col-md-5 mb-5">
+            <img src="../../project kelas 1/berita/Card (4).png">
+        </div>
+        <div class="col-md-5 mb-5 text-berita1">
+            <p>Dukungan penuh diutarakan oleh <strong>Penjabat (Pj.) Wali Kota Malang Dr. Ir. Wahyu Hidayat, MM</strong> atas beragam inovasi yang dikembangkan sekolah dalam memfasilitasi siswa istimewa untuk dapat mengenyam pendidikan setara, seperti salah satunya SIMBA ASIA.</p>
+        </div>
+        <div class="col-md-5 mb-5 text-berita2">
+           <p><strong>Purnawiyata kelas 9 SMPN 2 Malang  Tapel 2023 – 2024</strong>sukses diselenggarakan pada hari Minggu 10 Juni 2024 di Ascent Premiere Hotel and Convention, Kota Malang. Acara yang dimulai pada pukul 07.00 WIB ini mengambil tema  visi SMPN 2 Malang “Unggul Imtaq dan Iptek, berkarakter Pancasila, serta Peduli dan Berbudaya Lingkungan.</p>
+        </div>
+        <div class="col-md-5 mb-5 ">
+           <img src="../../project kelas 1/berita/image (5).png">
+        </div>
+        <div class="col-md-5 mb-5">
+            <img src="../../project kelas 1/berita/Card (5).png">
+        </div>
+        <div class="col-md-5 mb-5 text-berita1">
+            <p><strong>Tim Dewan Galang SMPN 2 Malang</strong> berhasil membawa 2 piala pada acara Dempo Cup XVIII 2023, dan mendapat juara sebagai Pinru terbaik pada tanggal 17 Oktober 2023</p>
+        </div>
+        <div class="col-md-5 mb-5 text-berita2">
+           <p>SMPN 2 Malang menerima peserta didik baru, dan pendaftaran terdapat 3 tahap : <br><br>
+
+            1. jalur afirmasi, kepindahan ortu & prestasi lomba (24 juni 2024 - 26 juni 2024)<br>
+            2. jalur prestasi nilai rapor (5 juli - 6 juli 2024)<br>
+            3. jalur zonasi (12 juli - 13 juli 2024)</p>
+        </div>
+        <div class="col-md-5 mb-5">
+           <img src="../../project kelas 1/berita/image (6).png">
+        </div>
+    </div>
+</div>
